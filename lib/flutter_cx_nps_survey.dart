3724137c5a4f3cd3997c352cad1ad08db7156c4e
Mark Banaria <markbanaria@Marks-MacBook-Pro.local>
2024-08-15 21:49:49 +0800

    add export of submission config

--- lib/flutter_cx_nps_survey.dart
+++ lib/flutter_cx_nps_survey.dart
@@ -1,6 +1,6 @@
 library flutter_cx_nps_survey;
 
-export 'src/survey_config.dart';
-export 'src/api_service.dart';
-export 'src/submission_manager.dart';
+export 'models/survey_config.dart';
+export 'models/submission_config.dart';
+export 'services/submission_manager.dart';
 export 'widgets/survey_form.dart';
